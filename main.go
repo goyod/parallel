e7c7c144666d0cddf224d0dbe6456d649e65163b
pallat <yod.pallat@gmail.com>
2020-02-01 22:48:33 +0700

    encrypt/decrypt

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"log"
+)
+
+func main() {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	enc := encrypt([]byte("exampleplaintext"))
+
+	fmt.Println(enc)
+
+	ciphertext, _ := hex.DecodeString("73c86d43a9d700a253a96c85b0f6b03ac9792e0e757f869cca306bd3cba1c62b")
+
+	fmt.Println(decrypt(ciphertext))
+}
+
+var key, _ = hex.DecodeString("6368616e676520746869732070617373")
+
+func encrypt(plaintext []byte) string {
+	// plaintext := []byte("exampleplaintext")
+
+	if len(plaintext)%aes.BlockSize != 0 {
+		panic("plaintext is not a multiple of the block size")
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
+
+	return fmt.Sprintf("%x", ciphertext)
+}
+
+func decrypt(ciphertext []byte) string {
+	// key, _ := hex.DecodeString("6368616e676520746869732070617373")
+	// ciphertext, _ := hex.DecodeString("73c86d43a9d700a253a96c85b0f6b03ac9792e0e757f869cca306bd3cba1c62b")
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		panic("ciphertext too short")
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	if len(ciphertext)%aes.BlockSize != 0 {
+		panic("ciphertext is not a multiple of the block size")
+	}
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+
+	mode.CryptBlocks(ciphertext, ciphertext)
+
+	return fmt.Sprintf("%s", ciphertext)
+}
